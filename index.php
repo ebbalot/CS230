fb39a1d13cf4359bf248c7b45cb1774f44464b47
ebbalot <epw0010@mix.wvu.edu>
2021-02-05 19:34:32 -0500

    lab2

---
+++ index.php
@@ -0,0 +1,51 @@
+<main>
+    <link rel="stylesheet" href="css/about.css" >
+
+    <div class = "about">
+        <h1>About Us
+            <p>bruh moment #2</p>
+        </h1>
+    </div>
+    <div class = "row">
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/1.jpeg">
+                <div class = "container">
+                    <h2>meme master</h2>
+                    <p>monkey man</p>
+                    <p>Email: banana79@monkey.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/2.jpg">
+                <div class = "container">
+                    <h2>monkey from kung fu panda</h2>
+                    <p>monkey from kung fu panda 2</p>
+                    <p>Email: jackiechanismyvoiceactor@monkey.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/3.jpg">
+                <div class = "container">
+                    <h2>Super Monkey Maker</h2>
+                    <p>Gear Monkey</p>
+                    <p>Email: makinmonkeys321@monkey.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/4.png">
+                <div class = "container">
+                    <h2>Clipart Monkey</h2>
+                    <p>Monkey with banan</p>
+                    <p>Email: banana77@monkey.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
