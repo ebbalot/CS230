143218c6bf9d7188e79e0b14e821d5781e31ea8c
ebbalot <epw0010@mix.wvu.edu>
2021-03-19 14:28:30 -0400

    Lab 7

--- includes/gallery-helper.php
+++ includes/gallery-helper.php
@@ -13,7 +13,7 @@ if (isset($_POST['gallery-submit'])) {
     $file_size = $file['size'];
 
     $title = $_POST['title'];
-    $descript = $POST['descript'];
+    $descript = $_POST['descript'];
 
 $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
